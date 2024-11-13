c0439a6ed118c4a52e7e82570eadeaad9f59ceb2
Ivan Dvornichenko <acsahir1990@mail.ru>
2024-11-13 23:29:40 +0300

    Initial commit

---
+++ src/js/game.js
@@ -0,0 +1,17 @@
+import ClassCharacter from'./domain.js';
+console.log(ClassCharacter);
+
+export default class Game {
+    start() {
+      console.log('game started');
+    }
+  }
+  
+  export class GameSavingData {
+  }
+  
+  export function readGameSaving() {
+  }
+  
+  export function writeGameSaving() {
+  }
